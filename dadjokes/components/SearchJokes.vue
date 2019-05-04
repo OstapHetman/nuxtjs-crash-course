a84fdfe87375def9438d3770590f86c0ef6f9ffd
OstapHetman <prickly4528@gmail.com>
2019-05-04 22:06:27 +0300

    search jokes

---
+++ dadjokes/components/SearchJokes.vue
@@ -0,0 +1,29 @@
+<template>
+  <form class="form-inline mt-2 mt-md-0" @submit.prevent="onSubmit">
+    <input
+      class="form-control mr-sm-2"
+      type="text"
+      placeholder="Search"
+      aria-label="Search"
+      v-model="text"
+    >
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SearchJokes",
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("search-text", this.text);
+      this.text = "";
+    }
+  }
+};
+</script>
--- dadjokes/pages/Jokes/index.vue
+++ dadjokes/pages/Jokes/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="container">
+    <div class="row my-5">
+      <div class="col-12">
+        <SearchJokes v-on:search-text="searchText"/>
+      </div>
+    </div>
     <div class="row">
       <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke"/>
     </div>
@@ -9,15 +14,33 @@
 <script>
 import axios from "axios";
 import Joke from "../../components/Joke";
+import SearchJokes from "../../components/SearchJokes";
 
 export default {
   components: {
-    Joke
+    Joke,
+    SearchJokes
   },
   data() {
     return {
       jokes: []
     };
+  },
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
+      const url = `https://icanhazdadjoke.com/search?term=${text}`;
+      try {
+        const res = await axios.get(url, config);
+        this.jokes = res.data.results;
+      } catch (err) {
+        console.log(err);
+      }
+    }
   },
   async created() {
     const config = {
